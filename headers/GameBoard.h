5c5a8ce57c1af6ec95b0377b21c8999ca0241136
TomBombadil <amiirfaridii@gmail.com>
2022-06-02 16:44:16 +0430

    Board Display Features

--- headers/GameBoard.h
+++ headers/GameBoard.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stack>
+#include <map>
 #include <iostream>
 
 #include "Piece.h"
--- src/GameBoard.cpp
+++ src/GameBoard.cpp
@@ -1,6 +1,9 @@
 #pragma once
 #include "../headers/GameBoard.h"
 
+map<int, char> convertIC = { {0, 'a'}, {1, 'b'}, {2, 'c'}, {3, 'd'}, {4, 'e'}, {5, 'f'}, {6, 'g'}, {7, 'h'} };
+map<char, int> convertCI = { {'a', 0}, {'b', 1}, {'c', 2}, {'d', 3}, {'e', 4}, {'f', 5}, {'g', 6}, {'h', 7} };
+
 void error(string message){
     cerr << message << endl;
     abort();
